9e75df6581e588c04415a2c05234a7d1c5fa2ae1
sunnz <1378551+sunnz@users.noreply.github.com>
2021-11-15 23:16:30 +1100

    feat: position main div

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
   setTimeout(() => setStart(new Date()), 990);
 
   return (
-    <div>
+    <div className="position-absolute vw-100 translate-middle top-50 start-50">
       <Head>
         <title>countdown</title>
         <link rel="icon" href="/favicon.ico" />
