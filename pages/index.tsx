884a1f33c689681a5ce3bff4310cf11c005e918f
sunnz <1378551+sunnz@users.noreply.github.com>
2021-11-18 23:42:20 +1100

    feat: add parrots for when metamon arrives

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import { Col, Container, Row } from "react-bootstrap";
 import { intervalToDuration } from "date-fns";
 import { useState } from "react";
+import parrotGif from "../public/parrot.gif";
 
 const startBefore = new Date("2021-11-13T23:00+11:00");
 const startAfter = new Date("2022-01-18T10:00+11:00");
@@ -16,6 +18,9 @@ const Home: NextPage = () => {
   const [start, setStart] = useState(new Date());
   const { days, hours, minutes, seconds } = intervalToDuration({ start, end });
   setTimeout(() => setStart(new Date()), 990);
+
+  // still counting down?
+  const isCountingDown = start < end;
 
   return (
     <div style={{ marginTop: "40vh" }}>
@@ -45,7 +50,20 @@ const Home: NextPage = () => {
         </Row>
         <Row>
           <Col>
-            <p className="text-muted">to go!</p>
+            <p className="text-muted">
+              {isCountingDown ? (
+                <>to go!</>
+              ) : (
+                <>
+                  <Image src={parrotGif} alt="yes!" height={50} width={50} />
+                  <Image src={parrotGif} alt="yes!" height={50} width={50} />
+                  <Image src={parrotGif} alt="yes!" height={50} width={50} />
+                  <Image src={parrotGif} alt="yes!" height={50} width={50} />
+                  <Image src={parrotGif} alt="yes!" height={50} width={50} />
+                  metamon&apos;s here!
+                </>
+              )}
+            </p>
           </Col>
         </Row>
       </Container>
